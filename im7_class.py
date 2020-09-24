edddb5051e7893e426cb6148f5db7c3cfcea1d53
tjburrows <37048572+tjburrows@users.noreply.github.com>
2020-09-24 12:29:36 -0400

    reformatting, adding comments

--- im7_class.py
+++ im7_class.py
@@ -1,35 +1,42 @@
+# Class for reading and writing IM7 files
+# Stripped down version of Alan Fleming's IM module https://bitbucket.org/fleming79/im/
+
 import ReadIM
 import sys
 import os
 import numpy as np
 from re import findall
 
+
 def map_D(D, a, b, dtype):
-    """ Generic mapping from coordinates to pixel position
+    """Generic mapping from coordinates to pixel position
     using slope a intersect b
     returns (D - b)/a
     """
     return np.round((D - b) / a).astype(dtype)
 
+
 def map_d(d, a, b, dtype):
-    """ Generic mapping from pixel position to coordinates
-        using slope a intersect b
-        returns d * a + b
+    """Generic mapping from pixel position to coordinates
+    using slope a intersect b
+    returns d * a + b
     """
-    return np.array(d * a + b, dtype=dtype) 
+    return np.array(d * a + b, dtype=dtype)
+
 
 def get_num(line):
-    listOfNumbers = findall('[0123456789.-]+', line)
+    listOfNumbers = findall("[0123456789.-]+", line)
     return list(map(float, listOfNumbers))
+
 
 class IM7:
     def __init__(self, loadfile):
         self.loadfile = loadfile
-        self.data = {'attributes':{},'I':None}
-        
+        self.data = {"attributes": {}, "I": None}
+
         self.readIM7()
-        self.dtype = self.data['I'].data.dtype
-    
+        self.dtype = self.data["I"].data.dtype
+
     def _get_Buffer_(self):
         """
         Get buffer from self.loadfile. Only valid
@@ -52,149 +59,161 @@ class IM7:
         try:
             return ReadIM.get_Buffer_andAttributeList(self.loadfile)
         except IOError:
-            raise IOError("Problem loading file: {0}.Message: ".format(self.loadfile, sys.exc_info()[1]) )
-            
+            raise IOError(
+                "Problem loading file: {0}.Message: {1}".format(self.loadfile, sys.exc_info()[1])
+            )
+
     def map_i(self, i):
         """ Map stored intensities to real intensities """
-        iScale = get_num(self.data['attributes']['_SCALE_I'])
+        iScale = get_num(self.data["attributes"]["_SCALE_I"])
         if not (iScale[0] == 1.0 and iScale[1] == 0.0):
             return map_d(i, iScale[0], iScale[1], self.dtype)
         else:
             return i
-        
+
     def map_I(self, I):
-        """ Map real intensities to stored  intensities
+        """Map real intensities to stored  intensities
         buffer.scaleI.factor should be set
         """
-        factor = self.data['buffer']['scaleI']['factor']
-        offset = self.data['buffer']['scaleI']['offset']
+        factor = self.data["buffer"]["scaleI"]["factor"]
+        offset = self.data["buffer"]["scaleI"]["offset"]
         if not (factor == 1.0 and offset == 0.0):
             return map_D(I, factor, offset, self.dtype)
         else:
             return np.array(I, dtype=self.dtype)
-    
+
     def _set_buffer(self, buff):
         buff = ReadIM.BufferTypeAlt(buff, False, immutable=True)
-        self.data['buffer']  = {}
-        ReadIM.extra.obj2dict(buff, self.data['buffer'])
-    
+        self.data["buffer"] = {}
+        ReadIM.extra.obj2dict(buff, self.data["buffer"])
+
     def readIM7(self):
         """ Load the image from the the buffer/file (for .IM7 files) """
 
         extn = os.path.splitext(self.loadfile)[1]
-        if not extn.lower() == '.im7':
+        if not extn.lower() == ".im7":
             return
 
         try:
             buff, atts = self._get_Buffer_andAttributeList()
             attdict = ReadIM.extra.att2dict(atts)
-            self.data['attributes'].update(attdict)
+            self.data["attributes"].update(attdict)
             vbuff, buff = ReadIM.extra.buffer_as_array(buff)
             if buff.image_sub_type > 0:
-                raise TypeError('buffer does not contain an image: type = %s' % buff.image_sub_type)
+                raise TypeError("buffer does not contain an image: type = %s" % buff.image_sub_type)
             self._set_buffer(buff)
             vbuff2 = self.map_i(vbuff)
-            self.data['I'] = np.ma.masked_equal(vbuff2, 0.0)
+            self.data["I"] = np.ma.masked_equal(vbuff2, 0.0)
 
         finally:
             env = dir()
-            if 'vbuff' in env:
-                del(vbuff)  # Yes you need to do this to stop memory leaks
-            if 'vbuff2' in env:
-                del(vbuff2)
-            if 'buff' in env:
+            if "vbuff" in env:
+                del vbuff  # Yes you need to do this to stop memory leaks
+            if "vbuff2" in env:
+                del vbuff2
+            if "buff" in env:
                 ReadIM.DestroyBuffer(buff)
-                del(buff)
-            if 'atts' in env:
+                del buff
+            if "atts" in env:
                 ReadIM.DestroyAttributeListSafe(atts)
-                del(atts)
-            if 'attdict' in env:
-                del(attdict)
-        
+                del atts
+            if "attdict" in env:
+                del attdict
+
     def update_attributes_with_scales(self):
         """ update self.attributes with scaling information """
-            #transfer buffer info from v. Useful for writing files.
+        # transfer buffer info from v. Useful for writing files.
 
         # purge the frame scales # probably develop a better way to handle this.
-        for p in [att for att in self.data['attributes'] if att.find('FrameScale')==0]:
-            self.data['attributes'].pop(p)
+        for p in [att for att in self.data["attributes"] if att.find("FrameScale") == 0]:
+            self.data["attributes"].pop(p)
 
-        for p in [att for att in self.data['attributes'] if att.find('_SCALE_')==0]:
-            self.data['attributes'].pop(p)
+        for p in [att for att in self.data["attributes"] if att.find("_SCALE_") == 0]:
+            self.data["attributes"].pop(p)
 
-        for a in self.data['buffer']:
-            if a.find('scale') == 0:
-                sc = self.data['buffer'][a]
-                
+        for a in self.data["buffer"]:
+            if a.find("scale") == 0:
+                sc = self.data["buffer"][a]
+
                 # parse the info from the buffer in suitable format
 
                 # only work for frame0 at present
 
-                _key = '_SCALE_%s' % a[-1].upper()
-                _val = '{0:0.6f} {1:0.6f}\n{2}\n\n'.format(
-                        sc['factor'], sc['offset'], sc['unit'].strip('\n').strip('\x00'))
-                self.data['attributes'][_key] = _val
+                _key = "_SCALE_%s" % a[-1].upper()
+                _val = "{0:0.6f} {1:0.6f}\n{2}\n\n".format(
+                    sc["factor"], sc["offset"], sc["unit"].strip("\n").strip("\x00")
+                )
+                self.data["attributes"][_key] = _val
 
-                val = '{0:0.6g}\n{1:0.6g}\n{2}\n'.format(
-                        sc['factor'], sc['offset'], sc['unit'].strip('\n').strip('\x00'))
-                key = 'FrameScale%s0' % a[-1].upper()
-                self.data['attributes'][key] = val
-    
+                val = "{0:0.6g}\n{1:0.6g}\n{2}\n".format(
+                    sc["factor"], sc["offset"], sc["unit"].strip("\n").strip("\x00")
+                )
+                key = "FrameScale%s0" % a[-1].upper()
+                self.data["attributes"][key] = val
+
     def _im7_writer(self, dst):
         """ write the current buffer as masked to dst """
 
-        buff = ReadIM.BufferTypeAlt(self.data['buffer'])
-        buff.isFloat = (self.dtype == np.float32)
+        buff = ReadIM.BufferTypeAlt(self.data["buffer"])
+        buff.isFloat = self.dtype == np.float32
 
         try:
             # DaVis objects
             buff, error_code = ReadIM.extra.createBuffer(buff)
-            
+
             if error_code > 1:
-                raise IOError('Error code %s=%s' % (ReadIM.ERROR_CODES[error_code], error_code))
+                raise IOError("Error code %s=%s" % (ReadIM.ERROR_CODES[error_code], error_code))
 
             vbuff, buff2 = ReadIM.extra.buffer_as_array(buff)
 
             self.update_attributes_with_scales()
-            atts = ReadIM.load_AttributeList(self.data['attributes'])
+            atts = ReadIM.load_AttributeList(self.data["attributes"])
 
-            vbuff[:]    = 0
-            setzero     = np.logical_not(self.data['I'].mask)
+            vbuff[:] = 0
+            setzero = np.logical_not(self.data["I"].mask)
 
-            vbuff[:]    =  self.map_I(self.data['I']) * setzero
+            vbuff[:] = self.map_I(self.data["I"]) * setzero
 
             # write from buffer
             err = ReadIM.WriteIM7(dst, True, buff2, atts.next)
 
             if err:
-                codes = codes = dict([(getattr(ReadIM.core,f),f) for f in dir(ReadIM.core) if f.find('IMREAD_ERR')==0])
+                codes = codes = dict(
+                    [
+                        (getattr(ReadIM.core, f), f)
+                        for f in dir(ReadIM.core)
+                        if f.find("IMREAD_ERR") == 0
+                    ]
+                )
                 if err == 1:
-                    print('Note that you cannot write into the top level directory for some strange reason')
-                raise IOError('Error writing file: %s=%s' % (err, codes[err]))
+                    print(
+                        "Note that you cannot write into the top level directory for some strange reason"
+                    )
+                raise IOError("Error writing file: %s=%s" % (err, codes[err]))
 
-            print('Created %s' % os.path.basename(dst))
+            print("Created %s" % os.path.basename(dst))
 
         # clean up
         finally:
             env = dir()
-            if 'vbuff 'in env:
-                del(vbuff)  # Yes you need to do this to stop memory leaks
-            if 'buff' in env:
+            if "vbuff " in env:
+                del vbuff  # Yes you need to do this to stop memory leaks
+            if "buff" in env:
                 ReadIM.DestroyBuffer(buff)
-                del(buff)
-            if 'buff2' in env:
+                del buff
+            if "buff2" in env:
                 ReadIM.DestroyBuffer(buff2)
-                del(buff2)
-            if 'atts' in env:
+                del buff2
+            if "atts" in env:
                 ReadIM.DestroyAttributeListSafe(atts)
-                del(atts)
+                del atts
 
     def writeIM7(self, dst):
-        """ write the data to a .im7 file
-            if frame is None dst assumes that dst is a directory
-            else
+        """write the data to a .im7 file
+        if frame is None dst assumes that dst is a directory
+        else
         """
-        ext = '.im7'
+        ext = ".im7"
         if not ext == dst[-4:].lower():
             dst += ext
         dst = os.path.abspath(dst)
--- time_subtract.py
+++ time_subtract.py
@@ -8,10 +8,10 @@ import numpy as np
 from dask.distributed import Client, LocalCluster
 
 # Path to IM7 files
-im7_path = './testdata'
+im7_path = "./testdata"
 
 # Path for temporary files
-temp_path = './temp'
+temp_path = "./temp"
 
 # Filter length
 n = 5
@@ -20,59 +20,80 @@ n = 5
 workers = 4
 
 # Memory limit per worker
-worker_mem = '2GB'
+worker_mem = "2GB"
 
+# Terminates dask client / cluster
 def cleanup(environment):
-    if 'client' in environment:
+    if "client" in environment:
         client.close()
-    if 'cluster' in environment:
+    if "cluster" in environment:
         cluster.close()
 
-def writeIM7(Idata, im7, newPath):
-        im7.data['I'] = np.ma.masked_array(Idata.astype(sampleDtype), mask=im7.data['I'].mask)
-        im7.writeIM7(newPath)
 
-if __name__ == '__main__':    
+# Write IM7 object with new image data
+def writeIM7(Idata, oldIM7, newPath):
+    oldIM7.data["I"] = np.ma.masked_array(Idata.astype(sampleDtype), mask=oldIM7.data["I"].mask)
+    oldIM7.writeIM7(newPath)
+
+
+if __name__ == "__main__":
     cleanup(dir())
-    
-    cluster = LocalCluster(n_workers=workers, memory_limit=worker_mem, processes=True, dashboard_address='127.0.0.1:8787', local_directory=temp_path)
+
+    # Initialize Dask
+    cluster = LocalCluster(
+        n_workers=workers,
+        memory_limit=worker_mem,
+        processes=True,
+        dashboard_address="127.0.0.1:8787",
+        local_directory=temp_path,
+    )
     client = Client(cluster)
     print(client)
-    
+
+    # Find IM7 files
     im7_path = os.path.abspath(im7_path)
-    filenames = glob.glob(os.path.join(im7_path, '*.im7'))
+    filenames = glob.glob(os.path.join(im7_path, "*.im7"))
     filenames = list(map(os.path.abspath, filenames))
-    
     if not filenames:
-        raise ValueError('No .im7 files found at %s' % im7_path)
-        
+        raise ValueError("No .im7 files found at %s" % im7_path)
     filenamesOnly = list(map(os.path.basename, filenames))
     numImages = len(filenames)
+
+    # Length of one side of filter
     l = (n - 1) / 2
+
+    # Load one image to get data shape and type
     sample1 = IM7(filenames[0])
-    sampleShape = sample1.data['I'].data.shape
-    sampleDtype = sample1.data['I'].data.dtype
+    sampleShape = sample1.data["I"].data.shape
+    sampleDtype = sample1.data["I"].data.dtype
+
+    # Lazy load IM7 files and convert to 3D array, 1st dimension time
     lazy_im7s = [dask.delayed(IM7)(file) for file in filenames]
-    lazy_arrays = [da.from_delayed(im7.data['I'].data, shape=sampleShape, dtype=sampleDtype) for im7 in lazy_im7s]
+    lazy_arrays = [
+        da.from_delayed(im7.data["I"].data, shape=sampleShape, dtype=sampleDtype)
+        for im7 in lazy_im7s
+    ]
     lazyStack = da.stack(lazy_arrays)
-    lazyStack = lazyStack.rechunk('auto')
-    
+    lazyStack = lazyStack.rechunk("auto")
+
     # Find minimum of n points along first axis
     slidingMinShape = (n, 1, 1, 1)
-    minimum = minimum_filter(lazyStack, size=slidingMinShape, mode='nearest')
+    minimum = minimum_filter(lazyStack, size=slidingMinShape, mode="nearest")
     newImages = lazyStack - minimum
-    
-    folder = 'SubOverTimeMin_sL=%d' % n
+
+    # Create folder with Davis name convention
+    folder = "SubOverTimeMin_sL=%d" % n
     folder = os.path.join(im7_path, folder)
     if not os.path.exists(folder):
         os.mkdir(folder)
-    
-    
+
+    # Create list of operations
     writes = []
     for i in range(0, lazyStack.shape[0]):
         newPath = os.path.join(im7_path, folder, filenamesOnly[i])
-        writes.append(dask.delayed(writeIM7)(newImages[i,:], lazy_im7s[i], newPath))
+        writes.append(dask.delayed(writeIM7)(newImages[i, :], lazy_im7s[i], newPath))
 
+    # Execute operations
     try:
         dask.compute(*writes)
         cleanup(dir())
